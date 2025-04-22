56a75899bee0cc8fed10c6b7cf785f9ca9db5ce2
root <root@ip-172-31-2-115.ec2.internal>
2025-04-22 09:22:27 +0000

    Updated terraform provider version to 5.95.0

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.95.0"  
+    }
+  }
+}
+
 resource "aws_ecs_cluster" "strapi" {
   name = "strapi-cluster"
 }
